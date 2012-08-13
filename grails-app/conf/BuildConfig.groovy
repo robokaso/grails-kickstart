359c4754d8028389d4232443b97f06f1ade196d2
Robert Kasanicky <robokaso@gmail.com>
2012-08-13 21:29:15 +0200

    plugin upgrades

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -73,9 +73,9 @@ grails.project.dependency.resolution = {
 		compile ":greenmail:1.3.2"
 		compile ':rest-client-builder:1.0.2'
 		
-		compile ':platform-core:1.0.M2'
+		compile ':platform-core:1.0.M3'
 		
-		compile ':cloud-foundry:1.2.2'
+		compile ':cloud-foundry:1.2.3'
 		compile ':cloud-support:1.0.11'
 
         runtime ":hibernate:$grailsVersion"
