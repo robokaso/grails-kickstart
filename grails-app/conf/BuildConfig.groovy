cd08bae60cfe1af7e4208794929e26cdf0f73ec1
Robert Kasanicky <robokaso@gmail.com>
2012-11-17 15:51:35 +0100

    plugin upgrades

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,8 +30,8 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
 	
-	final GEB_VERSION = "0.7.2"
-	final SELENIUM_VERSION = "2.25.0"
+	final GEB_VERSION = "0.9.0-RC-1"
+	final SELENIUM_VERSION = "2.26.0"
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
@@ -41,7 +41,7 @@ grails.project.dependency.resolution = {
 //		test "org.seleniumhq.selenium:selenium-firefox-driver:$SELENIUM_VERSION"
 //		test("org.seleniumhq.selenium:selenium-chrome-driver:$SELENIUM_VERSION")
 		
-		test "org.codehaus.geb:geb-spock:$GEB_VERSION"
+		test "org.gebish:geb-spock:$GEB_VERSION"
 		
 		compile "org.jadira.usertype:usertype.jodatime:1.9.1"
 		
@@ -54,12 +54,13 @@ grails.project.dependency.resolution = {
 		compile ':zipped-resources:1.0'
 		compile ':cache-headers:1.1.5'
 		
+		compile ":angular-scaffolding:1.0-SNAPSHOT"
 		compile ":angularjs-resources:1.0.2"
 		compile ":lesscss-resources:1.3.0.3"
-		compile ":jasmine-resources:0.1.1"
+//		compile ":jasmine-resources:0.1.1"
 		
 		compile ":twitter-bootstrap:2.2.1"
-        compile ":jquery:1.8.0"
+        compile ":jquery:1.8.3"
 		
 		compile ':fields:1.3'
 		
@@ -69,11 +70,12 @@ grails.project.dependency.resolution = {
 			excludes 'servlet-api'
 		}
 		compile ":joda-time:1.4"
-		compile ":mail:1.0"
-		compile ":greenmail:1.3.2"
+		compile ":mail:1.0.1"
+		compile ":greenmail:1.3.3"
 		compile ':rest-client-builder:1.0.2'
 		
-		compile ':platform-core:1.0.M6'
+		compile ':platform-core:1.0.M6.1'
+		compile ':events-push:1.0.M3'
 		
 		compile ':cloud-foundry:1.2.3'
 		compile ':cloud-support:1.0.11'
@@ -89,10 +91,11 @@ grails.project.dependency.resolution = {
 		
 		runtime ":runtime-logging:0.4"
 		
-		test ":spock:0.6"
+		test ":spock:0.7"
 		test ":geb:$GEB_VERSION"
 		test ":code-coverage:1.2.5"
-//		test ':functional-test-development:0.2'
+		test ':functional-test-development:0.9.3'
+		test ":remote-control:1.3"
 //		test ':auto-test:1.0.1'
 		
 		/* not maintained?
